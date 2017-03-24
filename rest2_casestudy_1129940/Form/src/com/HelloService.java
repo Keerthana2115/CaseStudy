c5703d31ece76f26bb4c8ca8d2449da82559d942
Keerthana2115 <keerthanapari21@gmail.com>
2017-03-24 18:36:46 +0530

    Add files via upload

---
+++ rest2_casestudy_1129940/Form/src/com/HelloService.java
@@ -0,0 +1,24 @@
+package com;
+
+import javax.ws.rs.FormParam;  
+import javax.ws.rs.POST;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.core.Response;  
+
+@Path("/product")  
+
+public class HelloService {
+
+	 @POST  
+	    @Path("/add")  
+	    public Response addUser(  
+	        @FormParam("id") int id,  
+	        @FormParam("name") String name,  
+	        @FormParam("price") float price) {  
+	   
+	        return Response.status(200)  
+	            .entity(" Product added successfuly!<br> Id: "+id+"<br> Name: " + name+"<br> Price: "+price)  
+	            .build();  
+	    }  
+	
+}
